6320efa47315d15c95826148e089665c42f4a7c9
MohamedNicer <mohamednicer.mn@gmail.com>
2022-01-26 16:36:17 +0100

    flan fertlan

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -33,10 +33,9 @@
                                 <div class="mt-3">
                                     <h4>{{$user->last_name}} {{ $user->first_name}}</h4>
                                     <p class="text-secondary mb-1">Full Stack Developer</p>
-                                    <p class="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
                                     @if($user->id != Auth()->user()->id)
                                     <button class="btn btn-primary">Follow</button>
-                                    <button class="btn btn-outline-primary">Message</button>
+                                    <button class="btn btn-primary">Message</button>
                                     @endif
                                 </div>
                             </div>
